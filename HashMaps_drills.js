478202a7c9345a1775e836d64e7d8a26f4af7873
quonn <qber83@gmail.com>
2019-05-20 12:03:32 -0700

    Added solutions for 1 -3/ initial commit

---
+++ HashMaps_drills.js
@@ -0,0 +1,99 @@
+const hash = require("./Hashmap");
+
+main = () => {
+
+    let lor = new hash();
+    lor.MAX_LOAD_RATIO = .5
+    lor.SIZE_RATIO = 3
+
+    lor.set("Hobbit", "Bilbo");
+    lor.set("Hobbit", "Frodo");
+    lor.set("Wizard", "Gandolf");
+    lor.set("Human", "Aragon");
+    lor.set("Elf", "Legolas");
+    lor.set("Maiar", "The Necromancer");
+    lor.set("Maiar", "Sauron");
+    lor.set("RingBearer", "Gollum");
+    lor.set("LadyOfLight", "Galadriel");
+    lor.set("HalfElven", "Arwen");
+    lor.set("Ent", "Treebeard");
+    console.log(lor.get("Maiar"))
+    console.log(lor.get("Hobbit"))
+    console.log(lor)
+}
+
+// const MAX_LOAD_RATIO = 0.5;
+
+// const SIZE_RATIO = 3;
+
+// main();
+
+//.1) Create a HashMap class
+//All items hashed
+//Sauron & Frodo
+//Final capacity is 24, because our initial size was 8 and our
+//SIZE_RATIO = 3 (8 * 3 === 24)
+
+//2. WhatDoesThisDo
+//Creates 2 new hash maps with 2 new nodes apeice
+//Should output 10, then 20
+
+// const WhatDoesThisDo = function(){
+//     let str1 = 'Hello World.';
+//     let str2 = 'Hello World.';
+//     let map1 = new hash();
+//     map1.set(str1,10);
+//     map1.set(str2,20);
+//     let map2 = new hash();
+//     let str3 = str1;
+//     let str4 = str2;
+//     map2.set(str3,20);
+//     map2.set(str4,10);
+
+//     console.log(map1.get(str2));
+//     console.log(map2.get(str4));
+// }
+
+// WhatDoesThisDo();
+
+hasher = (val, length) => {
+    return {val:val, index: val % length}
+}
+
+let hashVals = [ 5, 28, 19, 15, 20, 33, 12, 17, 10];
+
+// hashVals.forEach(val => {
+//     console.log(hasher(val,9))
+// })
+
+
+// .3) Demonstrate the undersranding of hash maps
+//array = [10, 22, 31, 4, 15, 28, 17, 88, 59]
+let openOrder = { "0": 22,
+              "1": 88,
+              "4": 4,
+              "5": 15,
+              "6": 59,
+              "7": 28,
+              "8": 17,
+              "9": 31,
+              "10":10
+}
+
+
+//array = [ 5, 28, 19, 15, 20, 33, 12, 17, 10]
+
+let sepearteOrder = {
+                "1": "28,19,10",
+                "2": 20,
+                "3": 12,
+                "5": 5,
+                "6": "15,33",
+                "8": 17
+} 
+
+hasher = (str) => {
+    console.log(str.length)
+}
+
+hasher("fred");
